5ceb4f90ab37db25e79056864a4d23be71a78738
Georgy Farniev <gfarniev@icloud.com>
2020-10-01 20:09:48 +0400

    add docs and other extra features

--- src/example.ts
+++ src/example.ts
@@ -5,6 +5,11 @@ const delay = (t: number) => new Promise(r => setTimeout(r, t));
 
 
 class Chainable extends Chaining<string[], { foo: string } > {
+  constructor(data: string[], params: { foo: string }) {
+    super(data, params)
+    this.parallel = true
+  }
+
   step1(d: number) {
     console.log('queued step1');
     return this.queue(async () => {
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,101 @@
+/**
+ * TODO:
+ * - Improve jsdoc (add references, more details, etc)
+ * - Add tests
+ */
+
+/**
+ * Worker function
+ */
 export type WorkerFn = () => Promise<void> | void
 
+/**
+ * Internal execution context
+ * @private
+ */
 interface Context <D, P> {
-  data: D,
-  params: P,
+  /* Initial data */
+  initialData: D
+
+  /* Data accumulator */
+  data: D
+
+  /* Optional context parameters */
+  params: P
+
+  /* Workers to execute */
   tasks: WorkerFn[]
+
+  /* Whether to execute in parallel or sequentially */
+  parallel: boolean
 }
 
+/**
+ * Helper for implementing async chainable classes
+ */
 export default class Chaining <D = any, P = any> {
   private readonly ctx: Context<D, P>
 
+  /**
+   * @param data - initial data where result will be stored. Think of it as accumulator for reducer
+   * @param params - optional parameters, useful as global context for function execution.
+   */
   constructor(data: D, params: P) {
     if (!data) throw new Error('data parameter is required')
 
-    this.ctx = { data, params, tasks: [] }
+    this.ctx = {
+      initialData: data,
+      data,
+      params,
+      tasks: [],
+      parallel: false
+    }
   }
 
-  get data() { return this.ctx.data; }
-  get params() { return this.ctx.params; }
+  /**
+   * @returns current stored data
+   */
+  protected get data() { return this.ctx.data; }
 
-  queue(fn: WorkerFn) {
+  /**
+   * @returns parameters
+   */
+  protected get params() { return this.ctx.params; }
+
+  /**
+   * Tells to execute chained methods in parallel (default is FIFO order)
+   */
+  protected set parallel(value: boolean) { this.ctx.parallel = value }
+
+  /**
+   * Adds function to tasks queue for further execution by exec() method
+   * @param fn - worker function. Should be bound to chain class and should take advantage on access
+   * to data and params props as necessary. Returns reference to this for convenience, so you can
+   * write like: return this.queue(async () => ...). Could be both synchronous or asynchronous
+   */
+  protected queue(fn: WorkerFn) {
     this.ctx.tasks.push(fn);
     return this;
   }
 
-  async exec() {
-    while(this.ctx.tasks.length) {
-      const next = this.ctx.tasks.shift();
-      if (next) await next();
+  /**
+   * Asynchronously executing functions added by queue method in FIFO order
+   * @returns data accumulated after execution
+   */
+  public async exec() {
+    // Reset accumulated data
+    this.ctx.data = this.ctx.initialData
+
+    if (this.ctx.parallel) {
+      await Promise.all(this.ctx.tasks.map(task => task()))
+    } else {
+      while(this.ctx.tasks.length) {
+        // We expect functon to be defined, otherwise it should be a runtime error
+        const next = this.ctx.tasks.shift() as WorkerFn;
+        await next();
+      }
     }
+
     return this.data;
   }
 }
